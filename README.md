f34be8ce5964f9761effb4d4497070424c0d5204
Vidor Kanalas <vidor.kanalas@gmail.com>
2016-10-24 00:01:07 +0200

    Retrieve album pictures, and start with the layout which will be used in routing

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # respotify
+
+
+## Useful commands
+
+To start the serving the page
+```
+npm run start
+```
--- src/index.js
+++ src/index.js
@@ -1,2 +1,44 @@
-const foo = 'bar';
-foo.toUpperCase();
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/SearchBar';
+import AlbumList from './components/AlbumList';
+import * as musicApi from './api/musicApi';
+
+
+import Layout from './pages/Layout'
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = ({
+      albums: [],
+    });
+    this.getAlbums = this.getAlbums.bind(this);
+    this.processAlbums = this.processAlbums.bind(this);
+  }
+
+  getAlbums(artist) {
+    musicApi.getAlbums(artist, this.processAlbums);
+  }
+
+  processAlbums(payload) {
+    this.setState({
+      albums: payload.albums.items,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Layout />
+        <SearchBar getAlbums={this.getAlbums} />
+        <AlbumList albums={this.state.albums} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('container')
+);
