afd9f5960bee25b2cc10c09b48a0253645137d73
Vidor Kanalas <vidor.kanalas@gmail.com>
2016-11-03 23:13:56 +0100

    Programatically find image size

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,20 +6,31 @@ import Img1 from '../../images/home/vide_sanitaire.jpg';
 
 export default class Home extends React.Component {
   render() {
+      var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth
+      var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
+      var imgStyle = {
+        width: windowWidth ,
+        height: windowHeight - 20 - 60 - 10,
+      }
     return (
-      <div>
-        <div className="container">
+        <div>
           <div className="row">
-            <div className="col-lg-12">
-              <h1>Hey</h1>
-                <img
-                  src={Img1}
-                  alt="Not Found Img1"
-                />
-            </div>
+            <img
+              src={Img1}
+              alt="Not Found Img1"
+              style={imgStyle} 
+            />
           </div>
         </div>
-      </div>
     );
   }
 }
+
+Home.styles = {
+  img: {
+    flex: 1,
+    width: 1200,
+    objectFit: 'cover',
+    backgroundSize: 'cover',  
+  },
+};
